58c16d0c7e75a820417e133d632933a2cc1952f5
turquetti <gabrielaturquetti@gmail.com>
2021-03-08 19:36:58 -0300

    starting module 2

---
+++ modulo_2/dicionario_projeto.py
@@ -0,0 +1,23 @@
+gabarito = {
+    "Pergunta 1": "B", 
+    "Pergunta 2": "A", 
+    "Pergunta 3": "B", 
+    "Pergunta 4": "C", 
+    "Pergunta 5": "A", 
+    "Pergunta 5 Alternativa": "B", 
+    "Pergunta 6": "B", 
+    "Pergunta 7": "B" 
+}
+
+if resposta_p1 == gabarito['Pergunta 1']:
+    print("Acertou!")
+else: 
+    print("Errou!")
+
+local = {
+    "Jacarezinho": "Porto de Fordlândia",
+    "Zé gotinha": "Posto de Saúde de Fordlândia",
+    "Mutantinho": "Escritório Central de Fordlândia"
+}
+
+print(local['Mutantinho'])
